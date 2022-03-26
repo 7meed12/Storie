807b2073225528f947df730e7cd9125a3e47513f
abdulhameedkhaled <abdulhameed.khaled12@gmail.com>
2022-03-27 01:50:19 +0200

    PictureUrl automapping handled , static content added

--- Api/Helpers/MappingProfile.cs
+++ Api/Helpers/MappingProfile.cs
@@ -10,7 +10,8 @@ namespace Api.Helpers
         {
             CreateMap<Product, ProductDto>()
                 .ForMember(x => x.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
-                .ForMember(x => x.ProductType, o => o.MapFrom(s => s.ProductType.Name));
+                .ForMember(x => x.ProductType, o => o.MapFrom(s => s.ProductType.Name))
+                .ForMember(x => x.PictureUrl, o => o.MapFrom<ProductUrlResvoler>());
         }
 
     }
---
+++ Api/Helpers/ProductUrlResolver.cs
@@ -0,0 +1,24 @@
+﻿using Api.Dto;
+using AutoMapper;
+using Models.Entities;
+
+namespace Api.Helpers
+{
+    
+        public class ProductUrlResvoler : IValueResolver<Product,ProductDto, string>
+        {
+            private readonly IConfiguration _config;
+
+            public ProductUrlResvoler(IConfiguration config)
+            {
+                _config = config;
+            }
+
+            public string Resolve(Product source, ProductDto destination, string destMember, ResolutionContext context)
+            {
+                if (!string.IsNullOrEmpty(source.PictureUrl)) return _config["ApiUrl"] + source.PictureUrl;
+                return null;
+            }
+        }
+ 
+}
